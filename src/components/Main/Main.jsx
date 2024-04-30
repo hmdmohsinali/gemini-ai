394f2f232f7f7cb26882eda038744671551e05ad
Mohsin A. <hmdmohsinali@gmail.com>
2024-04-30 14:24:09 +0500

    project completed with resposiveness

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -14,11 +14,17 @@ const Main = () => {
     input,
   } = useContext(Context);
 
-  const handleKeyDown = (e)=>{
-    if(e.key==='Enter'){
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
       onSent();
     }
-  }
+  };
+
+  const handleCardClick = (data) => {
+    setInput(data);
+
+    onSent(data);
+  };
 
   return (
     <div className="main min-h-lvh flex-1 pb-[15vh] relative ">
@@ -27,91 +33,118 @@ const Main = () => {
         <img className="w-[40px] rounded-[50%]" src={assets.user_icon} alt="" />
       </div>
       <div className="main-container max-w-[900px] m-auto">
-
-        {!showResult
-        ?<>
-        <div className="greet my-[50px] mx-[0px] text-[56px] text-[#c4c7c5] font-[500] p-[20px] ">
-          <p>
-            <span>Hello, Dev.</span>
-          </p>
-          <p>How can I help you today?</p>
-        </div>
-        <div className="cards grid grid-cols-4 gap-[15px] p-[20px]">
-          <div className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer hover:bg-[#dfe4ea]">
-            <p className="text-[#585858] text-[17px]">
-              Suggest beautiful places to see on an upcoming road trip
-            </p>
-            <img
-              className="w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]"
-              src={assets.compass_icon}
-              alt=""
-            />
-          </div>
-          <div className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer hover:bg-[#dfe4ea]">
-            <p className="text-[#585858] text-[17px]">
-              Briefly decribe this concpt: Urban planning
-            </p>
-            <img
-              className="w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]"
-              src={assets.bulb_icon}
-              alt=""
-            />
-          </div>
-          <div className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer hover:bg-[#dfe4ea]">
-            <p className="text-[#585858] text-[17px]">
-              Brainstorm team bonding activities for our work retreat
-            </p>
-            <img
-              className="w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]"
-              src={assets.message_icon}
-              alt=""
-            />
-          </div>
-          <div className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer hover:bg-[#dfe4ea]">
-            <p className="text-[#585858] text-[17px]">
-              Improve the radability of the following code
-            </p>
-            <img
-              className="w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]"
-              src={assets.code_icon}
-              alt=""
-            />
-          </div>
-        </div>
-        </>
-        :
-        <div className="result py-0 px-[5%] max-h-[70vh] overflow-y-scroll">
-          <div className="result-title my-[40px] mx-0 flex items-center gap-[20px]">
-            <img className="w-[40px] rounded-[50%]" src={assets.user_icon} alt="" />
-            <p>{recentPrompt}</p>
-          </div>
-          <div className="result-data flex items-start gap-[20px]">
-            <img src={assets.gemini_icon} alt="" />
-            {loading
-            ? 
-            <div className="loader w-[100%] flex flex-col gap-[10px]">
-              <hr />
-              <hr />
-              <hr />
+        {!showResult ? (
+          <>
+            <div className="greet my-[50px] mx-[0px] text-[56px] text-[#c4c7c5] font-[500] p-[20px] ">
+              <p>
+                <span>Hello, Dev.</span>
+              </p>
+              <p>How can I help you today?</p>
             </div>
-            :
-            <div dangerouslySetInnerHTML={{__html:resultData}} className="text-[17px] font-[300] leading-[1.8]"></div>
-          }
-            
+            <div className="cards grid grid-cols-4 gap-[15px] p-[20px]">
+              <div
+                className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer hover:bg-[#dfe4ea]"
+                onClick={() =>
+                  handleCardClick(
+                    "Suggest beautiful places to see on an upcoming road trip"
+                  )
+                }
+              >
+                <p className="text-[#585858] text-[17px]">
+                  Suggest beautiful places to see on an upcoming road trip
+                </p>
+                <img
+                  className="w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]"
+                  src={assets.compass_icon}
+                  alt=""
+                />
+              </div>
+              <div
+                className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer hover:bg-[#dfe4ea]"
+                onClick={() =>
+                  handleCardClick("Briefly decribe this concpt: Urban planning")
+                }
+              >
+                <p className="text-[#585858] text-[17px]">
+                  Briefly decribe this concpt: Urban planning
+                </p>
+                <img
+                  className="w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]"
+                  src={assets.bulb_icon}
+                  alt=""
+                />
+              </div>
+              <div
+                className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer hover:bg-[#dfe4ea]"
+                onClick={() =>
+                  handleCardClick(
+                    "Brainstorm team bonding activities for our work retreat"
+                  )
+                }
+              >
+                <p className="text-[#585858] text-[17px]">
+                  Brainstorm team bonding activities for our work retreat
+                </p>
+                <img
+                  className="w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]"
+                  src={assets.message_icon}
+                  alt=""
+                />
+              </div>
+              <div
+                className="card h-[200px] p-[15px] bg-[#f0f4f9] rounded-[10px] relative cursor-pointer hover:bg-[#dfe4ea]"
+                onClick={() =>
+                  handleCardClick(
+                    "Improve the radability of the following code"
+                  )
+                }
+              >
+                <p className="text-[#585858] text-[17px]">
+                  Improve the radability of the following code
+                </p>
+                <img
+                  className="w-[35px] p-[5px] absolute bg-white rounded-[20px] bottom-[10px] right-[10px]"
+                  src={assets.code_icon}
+                  alt=""
+                />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result py-0 px-[5%] max-h-[70vh] overflow-y-scroll">
+            <div className="result-title my-[40px] mx-0 flex items-center gap-[20px]">
+              <img
+                className="w-[40px] rounded-[50%]"
+                src={assets.user_icon}
+                alt=""
+              />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data flex items-start gap-[20px]">
+              <img src={assets.gemini_icon} alt="" />
+              {loading ? (
+                <div className="loader w-[100%] flex flex-col gap-[10px]">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <div
+                  dangerouslySetInnerHTML={{ __html: resultData }}
+                  className="text-[17px] font-[300] leading-[1.8]"
+                ></div>
+              )}
+            </div>
           </div>
-          
-        </div>
+        )}
 
-        }
-
-        
         <div className="main-bottom absolute bottom-0 w-[100%] max-w-[900ps] py-[0px] px-[20px] m-auto">
           <div className="search-box flex items-center justify-between gap-[20px] bg-[#f0f4f9] py-[10px] px-[20px] rounded-[20px]">
             <input
               className="flex-1 bg-transparent border-none outline-none text-[18px] p-[8px]"
               type="text"
               placeholder="Enter a prompt here"
-              onChange={(e)=>setInput(e.target.value)}
+              onChange={(e) => setInput(e.target.value)}
               value={input}
               onKeyDown={handleKeyDown}
             />
@@ -126,12 +159,14 @@ const Main = () => {
                 src={assets.mic_icon}
                 alt=""
               />
-              <img
-                className="w-[24px] cursor-pointer"
-                src={assets.send_icon}
-                alt=""
-                onClick={()=>onSent()}
-              />
+              {input ? (
+                <img
+                  className="w-[24px] cursor-pointer"
+                  src={assets.send_icon}
+                  alt=""
+                  onClick={() => onSent()}
+                />
+              ) : null}
             </div>
           </div>
           <p className="bottom-info text-[13px] my-[15px] mx-auto text-center font-[300]">
